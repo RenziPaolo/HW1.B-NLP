8e11cc137ae7eb05be95ff8c373cc519f6329be6
RenziPaolo <paolorenzi00.1@gmail.com>
2024-04-17 15:14:39 +0200

    weired error

--- src/dataset.py
+++ src/dataset.py
@@ -1,50 +1,65 @@
 import json
+import torch
 from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
+from transformers import BertTokenizer
+from torchtext.vocab import Vocab, vocab
+from collections import Counter
 
-def read_json_file(test):
-    """
-    Read JSON data from a file and return it as a Python dictionary.
-    
-    Args:
-    file_path (str): The path to the JSON file.
-    
-    Returns:
-    dict: The JSON data as a dictionary.
-    """
-    if test:
-        file_path = "../data/haspeede3-task1-test-data.jsonl"
-    else:
-        file_path = "../data/haspeede3-task1-test-data.jsonl"
-    try:
-        with open(file_path, 'r') as file:
-            data_list = [json.loads(line.strip()) for line in file]
-            return data_list
-    except FileNotFoundError:
-        print("File not found.")
-        return None
-    except json.JSONDecodeError:
-        print("Invalid JSON format.")
-        return None
-    
 class JSONLDataset(Dataset):
     """
     PyTorch dataset class constructed from a dictionary, ideally read from JSON Lines (JSONL) file.
     """
 
-    def __init__(self, data_dict):
+    def __init__(self, test: str, device: str) -> None: 
         """
         Initialize the dataset with a dictionary.
 
         Args:
-        data_dict (dict): Dictionary containing the data.
+        test (bool): True if the dataset is for testing, False otherwise.
         """
-        self.data_dict = data_dict
+        self.data_dict = self.read_json_file(test)
+        self.max_length = self.find_max_length()
+        self.device = device
+        self.tokenizedData = []
+        tokenizer = BertTokenizer.from_pretrained("dbmdz/bert-base-italian-xxl-cased")
+        for sample in self.data_dict:
+            text = sample['text']  # Assuming 'text' is the key for the text data
+            tokenizedText = [token for token in tokenizer.tokenize(text)]  # Tokenizing text
+            label = 0 if sample['label'] == 'si' else 1
+            self.tokenizedData.append({'text': tokenizedText, 'label': label})
+        self.indexedData = None
+
+    def read_json_file(self, test:  bool) -> list:
+        """
+        Read JSON data from a file and return it as a Python dictionary.
+
+        Args:
+        test (bool): True if the dataset is for testing, False otherwise.
+
+        Returns:
+        dict: The JSON data as a dictionary.
+        """
+        if test:
+            file_path = "../data/haspeede3-task1-test-data.jsonl"
+        else:
+            file_path = "../data/haspeede3-task1-test-data.jsonl"
+        try:
+            with open(file_path, 'r') as file:
+                data_list = [json.loads(line.strip()) for line in file]
+                return data_list
+        except FileNotFoundError:
+            print("File not found.")
+            return None
+        except json.JSONDecodeError:
+            print("Invalid JSON format.")
+            return None
 
     def __len__(self):
         """
         Return the number of samples in the dataset.
         """
-        return len(self.data_dict)
+        return len(self.tokenizedData)
 
     def __getitem__(self, idx):
         """
@@ -56,10 +71,85 @@ class JSONLDataset(Dataset):
         Returns:
         dict: Dictionary containing the sample data.
         """
-        return self.data_dict[idx]
+        return self.tokenizedData[idx]
+
+    def find_max_length(self):
+        """
+        Find the maximum sequence length in the dataset.
+
+        Returns:
+        int: Maximum sequence length.
+        """
+        max_length = 0
+        for sample in self.data_dict:
+            text = sample['text']  # Assuming 'text' is the key for the text data
+            length = len(text.split())
+            max_length = max(max_length, length)
+        return max_length
+    
+    def get_vocabulary(
+        self,
+        pad_token: str = "<pad>",
+        unk_token: str = "<unk>",
+        extra_tokens: list[str] = []
+    ) -> Vocab:
+        """Builds a `torchtext.vocab.Vocab` object from data stored in this object."""
+        # most_common() returns a list of (token, count) pairs, so we convert them back into dictionary
+        vocab_counter = dict(Counter(token for sent in self.tokenizedData for token in sent["text"]).most_common())
+        # We build the vocabulary through a dictionary like {token: frequency, ...}
+        vocabulary = vocab(vocab_counter, min_freq=1, specials=[pad_token, unk_token, *extra_tokens])
+        # vocabulary(list of tokens) returns a list of values, so get the only one
+        vocabulary.set_default_index(vocabulary([unk_token])[0])
+        return vocabulary
+    
+    def index(self, vocabulary: Vocab) -> None:
+        """Builds `self.indexedData` by converting raw samples to input_ids following `vocabulary`"""
+        if self.indexedData is not None:
+            print("Dataset has already been indexed. Keeping old index...")
+        else:
+            indexedData = []
+            for sample in self.tokenizedData:
+                # append the dictionary containing ids of the input tokens and label
+                indexedData.append({"input_ids": vocabulary(sample["text"]), "label": sample["label"]})
+            self.indexedData = indexedData
+            print("indexed!")
+
+    def indexBatch(self, vocabulary: Vocab, batch: list[dict]) -> None:
+        """Builds `self.indexedData` by converting raw samples to input_ids following `vocabulary`"""
+        if self.indexedData is not None:
+            print("Dataset has already been indexed. Keeping old index...")
+        else:
+            indexedData = []
+            for sample in batch:
+                # append the dictionary containing ids of the input tokens and label
+                indexedData.append({"input_ids": vocabulary(sample["text"]), "label": sample["label"]})
+        return indexedData
+
+
+    def _collate_fn(self, batch: list[dict]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Collate function to pad sequences within a batch.
+
+        Args:
+        batch (list): List of dictionaries, each containing a sample.
+
+        Returns:
+        dict: Dictionary containing padded sequences.
+        """
+        if "input_ids" not in batch[0].keys():
+            batch = self.indexBatch(self.get_vocabulary(), batch)
+        for sample in batch:
+                sequence_lengths = torch.tensor([len(sample["input_ids"])], dtype=torch.long)
+        labels = [sample['label'] for sample in batch]
+        padded_texts = pad_sequence( (
+                torch.tensor(sample["input_ids"], dtype=torch.long, device=self.device)
+                for sample in batch
+            ), batch_first=True)
+        return  sequence_lengths, padded_texts, labels
+
     
 # MAIN
 if __name__ == '__main__' :
-    test_json = read_json_file(True)
-    dataset = JSONLDataset(test_json)
+    dataset = JSONLDataset(test=True, device='cuda')
     print(dataset[0])
+    print(len(dataset.get_vocabulary().state_dict()))
--- src/model.py
+++ src/model.py
@@ -1,50 +1,78 @@
 import torch
 import torch.nn as nn
+from dataset import JSONLDataset
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.utils.data import DataLoader
 
 class BidirectionalLSTM(nn.Module):
     """
     Bidirectional LSTM model.
     """
 
-    def __init__(self, input_size, hidden_size, num_layers, output_size, device):
-        """
-        Initialize the Bidirectional LSTM model.
+    def __init__(
+        self,
+        vocabulary_length: int,
+        hidden_size: int,
+        num_layers: int,
+        bilstm_dropout: float,
+        padding_id: int,
+        device: str = "cuda",
+    ) -> None:
 
-        Args:
-        input_size (int): The number of expected features in the input.
-        hidden_size (int): The number of features in the hidden state.
-        num_layers (int): Number of recurrent layers.
-        output_size (int): The number of output features.
-        """
         super(BidirectionalLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
+        # Embedding layer
+        self.embedding = nn.Embedding(
+            num_embeddings=vocabulary_length,
+            embedding_dim=hidden_size,
+            padding_idx=padding_id, # avoid updating the gradient of padding entries
+            device=device
+        )
+        self.device = device
         # Bidirectional LSTM layer
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True, device=device)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True, bidirectional=True, dropout=bilstm_dropout, device=device)
 
         # Output layer
-        self.fc = nn.Linear(hidden_size * 2, output_size, device=device)
+        self.fc = nn.Linear(hidden_size * 2, 2, device=device)
 
-    def forward(self, x):
+    def forward(self, x: list[torch.Tensor, torch.Tensor]) -> torch.Tensor:
         """
         Forward pass of the Bidirectional LSTM model.
 
         Args:
-        x (torch.Tensor): Input tensor of shape (batch_size, seq_length, input_size).
+        x (torch.Tensor): Input tensor of shape (batch_size, input_size).
 
         Returns:
         torch.Tensor: Output tensor of shape (batch_size, output_size).
         """
+        print(x[1].shape)
+        # Get the different parts of the batch
+        input_ids, sequence_lengths = x
+
+        embeds = self.embedding(input_ids.to(self.device))
+        print(embeds.shape)
+        packed = pack_padded_sequence(embeds, sequence_lengths, batch_first=True, enforce_sorted=False)
+
         # Initialize hidden state with zeros
         h0 = torch.rand(self.num_layers * 2, x.size(0), self.hidden_size).to(x.device)
         # Initialize cell state with zeros
         c0 = torch.rand(self.num_layers * 2, x.size(0), self.hidden_size).to(x.device)
 
         # Forward propagate LSTM
-        out, _ = self.lstm(x, (h0, c0))  # out shape: (batch_size, seq_length, hidden_size * 2)
+        out, _ = self.lstm(packed, (h0, c0))  # out shape: (batch_size, seq_length, hidden_size * 2)
 
         # Decode the hidden state of the last time step
         out = self.fc(out[:, -1, :])  # out shape: (batch_size, output_size)
 
         return out
+    
+# MAIN
+if __name__ == '__main__' :
+    dataset = JSONLDataset(test=True, device='cuda')
+    dataloader = DataLoader(dataset, batch_size=128, shuffle=True, collate_fn=dataset._collate_fn)
+    lstm = BidirectionalLSTM(len(dataset.get_vocabulary()), 128, 4, 0, 0, 'cuda')
+    for step, (sequence_lengths, inputs, labels) in enumerate(dataloader):
+
+        predictions = lstm((sequence_lengths, inputs))
